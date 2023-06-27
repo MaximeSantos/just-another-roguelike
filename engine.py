64188de3e3a96416347e41c005dd6aedf4c9262a
MaximeSantos <118924091+MaximeSantos@users.noreply.github.com>
2023-06-27 17:22:14 +0200

    Part 4 - Add Field Of View to our Engine and implement it to our GameMap

--- engine.py
+++ engine.py
@@ -2,6 +2,7 @@ from typing import Set, Iterable, Any
 
 from tcod.context import Context
 from tcod.console import Console
+from tcod.map import compute_fov
 
 from entity import Entity
 from game_map import GameMap
@@ -20,6 +21,7 @@ class Engine:
         self.event_handler = event_handler
         self.game_map = game_map
         self.player = player
+        self.update_fov()  # the function is actually called when Engine is instantiated, before the player has even moved
 
     def handle_event(self, events: Iterable[Any]) -> None:
         for event in events:
@@ -30,9 +32,29 @@ class Engine:
 
             action.perform(self, self.player)
 
+            self.update_fov()  # updates the FOV before the player's next action
+
+    def update_fov(self) -> None:
+        """Recompute the visible area based on the players point of view."""
+        # compute_fov will return an array of visible tiles
+        # https://python-tcod.readthedocs.io/en/latest/tcod/map.html#tcod.map.compute_fov for the complete documentation
+        self.game_map.visible[:] = compute_fov(
+            # transparency : takes a 2D numpy array, and considers any non-zero values to be transparent
+            self.game_map.tiles["transparent"],
+            # pov : origin point for the FOV
+            (self.player.x, self.player.y),
+            # radius : distance to which the FOV extends
+            radius=8,
+        )
+        # if a tile is "visible" it should be added to "explored"
+        # sets the explored array to include everything in the visible array, plus whatever it already had
+        self.game_map.explored |= self.game_map.visible
+
     def render(self, console: Console, context: Context) -> None:
         self.game_map.render(console)
         for entity in self.entities:
-            console.print(entity.x, entity.y, entity.char, fg=entity.color)
+            # only prints entity within the FOV
+            if self.game_map.visible[entity.x, entity.y]:
+                console.print(entity.x, entity.y, entity.char, fg=entity.color)
         context.present(console)
         console.clear()
--- game_map.py
+++ game_map.py
@@ -7,13 +7,29 @@ import tile_types
 class GameMap:
     def __init__(self, width: int, height: int):
         self.width, self.height = width, height
-        # create a 2D array, filled with the same values, here walls
+        # create 2D arrays, filled with the same values, walls by default, or False for our visible & explored tiles
         self.tiles = np.full((width, height), fill_value=tile_types.wall, order="F")
+        self.visible = np.full((width, height), fill_value=False, order="F")
+        self.explored = np.full((width, height), fill_value=False, order="F")
 
     def in_bounds(self, x: int, y: int) -> bool:
         """Return True if x and y are inside of the bounds of this map."""
         return 0 <= x < self.width and 0 <= y < self.height
 
     def render(self, console: Console) -> None:
+        """
+        Renders the map.
+
+        If a tile is in the "visible" array, then draw it with the "light" colors.
+        If it isn't, but it's in the "explored" array, then draw it with the "dark" colors.
+        Otherwise, the default is "SHROUD".
+        """
         # console.rgb much faster than console.print to render entire map
-        console.rgb[0 : self.width, 0 : self.height] = self.tiles["dark"]
+        console.rgb[0 : self.width, 0 : self.height] = np.select(
+            # condlist sets the conditions, first checks if tile is visible, then if it is explored
+            condlist=[self.visible, self.explored],
+            # choicelist selects the proper value depending on if it passed the first or second condition
+            choicelist=[self.tiles["light"], self.tiles["dark"]],
+            # if it passed neither, then it gets the default value in SHROUD
+            default=tile_types.SHROUD,
+        )
